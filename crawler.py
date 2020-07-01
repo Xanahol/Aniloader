beb1031be798a2151b128f4b1f72ec2437422de4
Xana Holovous <45090939+Xanahol@users.noreply.github.com>
2020-07-01 14:10:54 +0200

    Build 1.0

--- crawler.py
+++ crawler.py
@@ -28,20 +28,23 @@ def go_to_anime(name):
 
 
 def show_all_episodes():
-    #i = 0
-    clickable = WebDriverWait(hs_driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//*[@class='more-button']")))
-    el = hs_driver.find_element_by_xpath("//*[@class='more-button']")
-    elattr = el.get_attribute('href')
-    print(elattr)
-    tryme = "#" in elattr
 
-    if tryme == True:
-        hs_driver.implicitly_wait(3)
-        ActionChains(hs_driver).click(clickable).perform()
+    while True:
 
-    else:
-        pass
+        try:
+            el = hs_driver.find_element_by_xpath("//*[@class='more-button']")
+        except:
+            break
+        elattr = el.get_attribute('href')
+        tryme = "#" in elattr
+
+        if tryme == True:
+            clickable = WebDriverWait(hs_driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, "//*[@class='more-button']")))
+            ActionChains(hs_driver).click(clickable).perform()
+
+        else:
+            break
 
 
 def get_magnet_links():
@@ -50,9 +53,14 @@ def get_magnet_links():
         "//div[@class='hs-shows']/div")
     a = 1
     for episode in episodes:
-        link = hs_driver.find_element_by_xpath(
-            "//div[@id='{}-1080p']/span[@class='dl-type hs-magnet-link']/a[@title='Magnet Link']".format(str(a).zfill(2))).get_attribute("href")
+        try:
+            link = hs_driver.find_element_by_xpath(
+                "//div[@id='{}-1080p']/span[@class='dl-type hs-magnet-link']/a[@title='Magnet Link']".format(str(a).zfill(2))).get_attribute("href")
+        except:
+            link = hs_driver.find_element_by_xpath(
+                "//div[@id='{}-720p']/span[@class='dl-type hs-magnet-link']/a[@title='Magnet Link']".format(str(a).zfill(2))).get_attribute("href")
         magnet_links.append(link)
+        a += 1
     return magnet_links
 
 
@@ -78,7 +86,7 @@ def log_in():
     ActionChains(torrent_driver).click(submit).perform()
 
 
-def insert_links(link_list):
+def open_add_link_interface():
     download = WebDriverWait(torrent_driver, 10).until(
         EC.presence_of_element_located((By.XPATH, "//*[@id='downloadButton']")))
     ActionChains(torrent_driver).click(download).perform()
@@ -86,23 +94,44 @@ def insert_links(link_list):
     torrent_driver.switch_to_frame(
         torrent_driver.find_element_by_id("downloadPage_iframe"))
 
+
+def insert_links(link_list):
+
     WebDriverWait(torrent_driver, 10).until(
         EC.presence_of_element_located((By.XPATH, "//*[@id='urls']")))
     element_linkbox = torrent_driver.find_element_by_xpath("//*[@id='urls']")
+
     for link in link_list:
+
         element_linkbox.send_keys(link)
         element_linkbox.send_keys(Keys.ENTER)
+
     submit = WebDriverWait(torrent_driver, 10).until(
         EC.presence_of_element_located((By.XPATH, "//*[@id='submitButton']")))
     ActionChains(torrent_driver).click(submit).perform()
 
 
+def insert_downloadpath(path):
+    WebDriverWait(torrent_driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//*[@id='savepath']")))
+    element_savepath = torrent_driver.find_element_by_xpath(
+        "//*[@id='savepath']")
+    element_savepath.clear()
+    element_savepath.send_keys(path)
+
+
 def crawl():
     connect_to_horriblesubs()
+
     open_qbittorrent()
     log_in()
+
     anime = userhandler.ask_for_anime()
     go_to_anime(anime)
     show_all_episodes()
     magnet_links = get_magnet_links()
+
+    open_add_link_interface()
+    download_path = userhandler.ask_for_downloadpath()
+    insert_downloadpath(download_path)
     insert_links(magnet_links)
--- userhandler.py
+++ userhandler.py
@@ -1,2 +1,5 @@
 def ask_for_anime():
     return input("Which anime would you like to download?\n")
+
+def ask_for_downloadpath():
+    return input("Where would you like to store the the files?\n")
