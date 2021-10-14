33ac87bdd07f2ad8b754b515d5b952193592eb7d
Xana Holovous <45090939+Xanahol@users.noreply.github.com>
2021-10-14 12:05:55 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
-ip_4 = "192.168.1.120" 
+#Example
+ip_4 = "192.168.4.162" 
 torrent_port = "8080"
-directories = ['F:\Plex\Anime','G:\Plex\Anime','E:\Plex\Anime']
+directories = ['C:\Example\Plex\Anime', 'D:\Example\Path\To\Plex\Anime']
 
-blacklist = ['Black Clover', 'Boruto - Naruto Next Generations', 'Detective Conan', 'One Piece', 'Boku no Hero Academia']
+# Not to Download
+blacklist = []
+# Not to standardize
 black = []
